b02e11ee2b0a84212b7c4b705e724ebf03c2752b
Xadoy <460335788@qq.com>
2017-06-07 18:49:54 -0400

    added a rulesCorrector to correct the property's name of a dollar mark

--- scanner.js
+++ scanner.js
@@ -12,28 +12,52 @@ function getRules(callback) {
     });
   });
 }
-function enumGenerator(rules){
-  var enumResult={};
+
+function enumGenerator(rules) {
+  var enumResult = {};
   var numOfRules = rules.length;
-  for (let i = 0; i < numOfRules; i++){
+  for (let i = 0; i < numOfRules; i++) {
     enumResult[rules[i].state] = i;
   }
   return enumResult;
 }
+
+// to correct the dollar marks caused by mongodb's restriction
+function rulesCorrector(original) {
+  var rules = original.rules;
+
+  var dollarMarkReplacement = original.dollarMarkReplacement;
+  var correctedRules = [];
+  for (let aRule of rules) {
+    if ('next' in aRule)
+      if (dollarMarkReplacement in aRule.next) {
+        aRule.next['$'] = aRule.next[dollarMarkReplacement];
+        delete aRule.next[dollarMarkReplacement];
+      }
+  }
+  return rules;
+}
+
 class scanner {
   constructor(callback) {
     var self = this;
     getRules(function(result) {
-      self.rules = result.rules;
+      self.rules = rulesCorrector(result);
       self.startIndex = result.startIndex;
       self.endlineIndex = result.endlineIndex;
       self.newlineIndex = result.newlineIndex;
       self.dollarMarkReplacement = result.dollarMarkReplacement;
       self.TOKENTYPE = enumGenerator(result.rules);
-      self.scan = function(string){
-        return [];
-      }
-      callback();
+      self.scan = function(string) {
+        var tokens = [];
+
+        // this function only scans the first variable which must be a string
+        // todo: maybe use throw
+        if (typeof string != 'string') return tokens;
+
+
+        return tokens;
+      } callback(self.TOKENTYPE, self.scan);
     })
   }
 }
